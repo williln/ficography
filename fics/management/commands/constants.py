9e75165d11fe5854b706c034ca7d8f8d3fa9c5e1
Lacey Williams Henschel <laceynwilliams@gmail.com>
2022-11-10 16:31:32 -0800

    Sample data constants

---
+++ fics/management/commands/constants.py
@@ -0,0 +1,86 @@
+HARRY_POTTER = "Harry Potter - J. K. Rowling"
+STAR_WARS = "Star Wars - All Media Types"
+SPY_FAMILY = "SPY x FAMILY (Anime)"
+GILMORE_GIRLS = "Gilmore Girls"
+
+HARRY_POTTER_CHARACTERS = [
+    "Harry Potter",
+    "Hermione Granger",
+    "Draco Malfoy",
+    "Ron Weasley",
+    "Sirius Black",
+    "Remus Lupin",
+    "Severus Snape",
+    "James Potter",
+    "Ginny Weasley",
+    "Albus Dumbledore",
+]
+
+HARRY_POTTER_SHIPS = [
+    "Draco Malfoy/Harry Potter",
+    "Sirius Black/Remus Lupin",
+    "Hermione Granger/Draco Malfoy",
+    "Hermione Granger/Ron Weasley",
+    "Harry Potter/Ginny Weasley",
+]
+
+STAR_WARS_CHARACTERS = [
+    "Rey (Star Wars)",
+    "Obi-Wan Kenobi",
+    "Ben Solo | Kylo Ren",
+    "Leia Organa",
+    "Armitage Hux",
+    "Poe Dameron",
+    "Anakin Skywalker",
+    "Luke Skywalker",
+    "Padme Amidala",
+]
+
+STAR_WARS_SHIPS = [
+    "Rey/Ben Solo | Kylo Ren",
+    "Padme Amidala/Anakin Skywalker",
+]
+
+SPY_CHARACTERS = [
+    "Loid Forger | Twilight",
+    "Anya Forger",
+    "Yor Briar Forger | Thorn Princess",
+]
+
+SPY_SHIPS = ["Loid Forger | Twilight/Yor Briar Forger | Thorn Princess"]
+
+GILMORE_CHARACTERS = [
+    "Rory Gilmore",
+    "Lorelai Gilmore",
+    "Luke Danes",
+    "Jess Mariano",
+    "Paris Geller",
+    "Logan Huntzberger",
+    "Emily Gilmore",
+    "Richard Gilmore",
+    "Dean Forester",
+    "Lane Kim",
+]
+
+GILMORE_SHIPS = [
+    "Rory Gilmore/Jess Mariano",
+    "Luke Danes/Lorelai Gilmore",
+    "Rory Gilmore/Logan Huntzberger",
+    "Paris Geller/Rory Gilmore",
+]
+
+FANDOMS = [HARRY_POTTER, STAR_WARS, SPY_FAMILY, GILMORE_GIRLS]
+
+CHARACTERS = {
+    HARRY_POTTER: HARRY_POTTER_CHARACTERS,
+    STAR_WARS: STAR_WARS_CHARACTERS,
+    SPY_FAMILY: SPY_CHARACTERS,
+    GILMORE_GIRLS: GILMORE_CHARACTERS,
+}
+
+SHIPS = {
+    HARRY_POTTER: HARRY_POTTER_SHIPS,
+    STAR_WARS: STAR_WARS_SHIPS,
+    SPY_FAMILY: SPY_SHIPS,
+    GILMORE_GIRLS: GILMORE_SHIPS,
+}
