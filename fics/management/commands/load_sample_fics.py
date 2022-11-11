d8cee14e5b2e7d53cd48a1451ce0946e82b4b969
Lacey Williams Henschel <laceynwilliams@gmail.com>
2022-11-10 16:32:47 -0800

    Split out sample data creation into functions and add tests

--- fics/management/commands/load_sample_fics.py
+++ fics/management/commands/load_sample_fics.py
@@ -1,91 +1,119 @@
+from datetime import timedelta
+import random
+import string
 import djclick as click
 from faker import Faker
 from model_bakery import baker
 
-from fics.models import Author, Character, CustomTag, Fandom, Ship
+from fics.models import Author, Character, CustomTag, Fandom, Ship, Fic
+
+from .constants import (
+    HARRY_POTTER,
+    STAR_WARS,
+    SPY_FAMILY,
+    GILMORE_GIRLS,
+    FANDOMS,
+    CHARACTERS,
+    SHIPS,
+)
 
 
 fake = Faker()
 
-FANDOMS = [
-    "Harry Potter - J. K. Rowling",
-    "Star Wars - All Media Types",
-    "SPY x FAMILY (Anime)",
-    "Gilmore Girls"
-]
 
-HARRY_POTTER_CHARACTERS = [
-    "Harry Potter",
-    "Hermione Granger",
-    "Draco Malfoy",
-    "Ron Weasley",
-    "Sirius Black",
-    "Remus Lupin",
-    "Severus Snape",
-    "James Potter",
-    "Ginny Weasley",
-    "Albus Dumbledore"
-]
+def create_authors(count: int):
+    """
+    Generate the number of fake authors in count
 
-HARRY_POTTER_SHIPS = [
-    "Draco Malfoy/Harry Potter",
-    "Sirius Black/Remus Lupin",
-    "Hermione Granger/Draco Malfoy",
-    "Hermione Granger/Ron Weasley",
-    "Harry Potter/Ginny Weasley"
-]
+    Returns queryset of the Authors that were created.
+    """
+    ids = []
+    for i in range(count):
+        obj, created = Author.objects.get_or_create(
+            username=fake.user_name(), url=fake.uri()
+        )
+        if created:
+            click.secho(f"---Created author: {obj.username}")
+            ids.append(obj.id)
 
-STAR_WARS_CHARACTERS = [
-    "Rey (Star Wars)",
-    "Obi-Wan Kenobi",
-    "Ben Solo | Kylo Ren",
-    "Leia Organa",
-    "Armitage Hux",
-    "Poe Dameron",
-    "Anakin Skywalker",
-    "Luke Skywalker",
-    "Padme Amidala"
-]
+    return Author.objects.filter(id__in=ids)
 
-STAR_WARS_SHIPS = [
-    "Rey/Ben Solo | Kylo Ren",
-    "Padme Amidala/Anakin Skywalker",
-]
 
-SPY_CHARACTERS = [
-    "Loid Forger | Twilight",
-    "Anya Forger",
-    "Yor Briar Forger | Thorn Princess"
-]
+def create_characters(characters: list):
+    """
+    Generate the Character objects based on the list of strings in characters
 
-SPY_SHIPS = ["Loid Forger | Twilight/Yor Briar Forger | Thorn Princess"]
+    Returns a queryset of the Characters that were created.
+    """
+    ids = []
+    for character in characters:
+        obj, created = Character.objects.get_or_create(name=character)
+        if created:
+            print(f"---Created character: {character}")
+            ids.append(obj.id)
 
-GILMORE_CHARACTERS = [
-    "Rory Gilmore",
-    "Lorelai Gilmore",
-    "Luke Danes",
-    "Jess Mariano",
-    "Paris Geller",
-    "Logan Huntzberger",
-    "Emily Gilmore",
-    "Richard Gilmore",
-    "Dean Forester",
-    "Lane Kim"
-]
-GILMORE_SHIPS = [
-    "Rory Gilmore/Jess Mariano",
-    "Luke Danes/Lorelai Gilmore",
-    "Rory Gilmore/Logan Huntzberger",
-    "Paris Geller/Rory Gilmore"
-]
+    return Character.objects.filter(id__in=ids)
+
+
+def create_fandoms(fandoms: list):
+    """
+    Generate the Fandom objects based on the list of strings in fandoms.
+
+    Returns queryset of the Fandoms that were created.
+    """
+    ids = []
+    for fandom in fandoms:
+        obj, created = Fandom.objects.get_or_create(name=fandom)
+        if created:
+            click.secho(f"---Created fandom: {fandom}")
+            ids.append(obj.id)
+
+    return Fandom.objects.filter(id__in=ids)
+
+
+def create_ships(ships: list):
+    """
+    Generate the Ship objects based on the list of strings in fandoms.
+
+    Returns queryset of the Ships that were created.
+    """
+    ids = []
+    for ship in ships:
+        obj, created = Ship.objects.get_or_create(name=ship)
+        if created:
+            click.secho(f"---Created ship: {ship}")
+            ids.append(obj.id)
+
+    return Ship.objects.filter(id__in=ids)
+
+
+def create_external_id() -> str:
+    """Returns a 10-digit string"""
+    result = string.digits
+    return "".join(random.choice(result) for i in range(10))
+
+
+def create_url(external_id: str) -> str:
+    """Takes a string. Returns an example URL with that string."""
+    return f"https://example.com/fics/{external_id}/"
+
+
+def get_ships_from_fandom(fandom: str, ships) -> list:
+    """Takes a fandom str, returns the queryset of ships for that fandom"""
+    ships = SHIPS.get(fandom)
+    # breakpoint()
+    if not ships:
+        raise Exception(f"No ships found for {fandom}")
+    return ships.filter(name__in=ships)
+
 
 
 @click.command()
 def command():
-    click.secho("Hello", fg='red')
+    click.secho("Hello", fg="red")
 
     # Create tags
-    click.secho("Creating tags...", fg='green')
+    click.secho("Creating tags...", fg="green")
     with open("fics/management/commands/tags.txt", "r") as f:
         lines = f.readlines()
         for tag in lines:
@@ -96,46 +124,30 @@ def command():
     tags = CustomTag.objects.all()
 
     # Create authors
-    click.secho("Creating authors...", fg='green')
-    # for i in range(30):
-    #     author, created = Author.objects.get_or_create(username=fake.user_name(), url=fake.uri())
-    #     if created:
-    #         click.secho(f"---Created author: {author.username}")
-
-    authors = Author.objects.all()
+    # click.secho("Creating authors...", fg="green")
+    # authors = create_authors(30)
 
     # Create fandoms
-    click.secho("Creating fandoms...", fg='green')
-    for fandom in FANDOMS:
-        _, created = Fandom.objects.get_or_create(name=fandom)
-        if created:
-            click.secho(f"---Created fandom: {fandom}")
-
-    fandoms = Fandom.objects.all()
+    click.secho("Creating fandoms...", fg="green")
+    fandoms = create_fandoms(FANDOMS)
 
     # Create ships
-    click.secho("Creating ships...", fg='green')
-    for ship in HARRY_POTTER_SHIPS + STAR_WARS_SHIPS + SPY_SHIPS + GILMORE_SHIPS:
-        _, created = Ship.objects.get_or_create(name=ship)
-        if created:
-            click.secho(f"---Created ship: {ship}")
+    click.secho("Creating ships...", fg="green")
+    ships = create_ships(SHIPS)
 
     # Create characters
-    click.secho("Creating characters...", fg='green')
-    for character in HARRY_POTTER_CHARACTERS + STAR_WARS_CHARACTERS + SPY_CHARACTERS + GILMORE_CHARACTERS:
-        _, created = Character.objects.get_or_create(name=character)
-        if created:
-            print(f"---Created character: {character}")
+    click.secho("Creating characters...", fg="green")
+    characters = create_characters(CHARACTERS)
 
-    characters = Character.objects.all()
 
-    click.secho("Creating fics...", fg='green')
 
-    click.secho("Creating Harry Potter fics...", fg='green')
+    click.secho("Creating fics...", fg="green")
+
+    click.secho("Creating Harry Potter fics...", fg="green")
     fandom = fandoms.filter(name__icontains="Harry").first()
 
     # Get titles
-    click.secho("Retrieving titles...", fg='green')
+    click.secho("Retrieving titles...", fg="green")
     with open("fics/management/commands/titles.txt", "r") as f:
         lines = f.readlines()
         titles = [title for title in lines if title]
@@ -156,5 +168,52 @@ def command():
         fic_characters = characters.objects.filter(name__in=HARRY_POTTER_CHARACTERS)
         fic_ships = ships.objects.filter(name__in=HARRY_POTTER_SHIPS)
 
-        # fic, created =
 
+
+
+
+
+
+def create_fic(data: dict) -> Fic:
+    """
+    Takes a data dictionary. Returns a Fic object.
+
+    Expects this format for `data`:
+    {
+        "title": str,
+        "fandoms": [Fandom],
+        "authors": [Author],
+        "ships": [Ship],
+        "characters": [Character],
+    }
+    """
+    external_id = create_external_id()
+    url = create_url(external_id)
+    date_published = fake.date_time_this_century()
+    date_updated = date_published + timedelta(days=random.randint(1, 30))
+    fic, created = Fic.objects.get_or_create(
+        title=data.get("title"),
+        defaults={
+            "url": data.get("url"),
+            "external_id": external_id,
+            "url": url,
+            "complete": random.choice([True, False]),
+            "rating": random.choice(Fic.Rating.choices[0]),
+            "summary": fake.paragraph(),
+            "date_published": date_published,
+            "date_updated": date_updated,
+        },
+    )
+    for fandom in data.get("fandoms"):
+        fic.fandoms.add(fandom)
+
+    for author in data.get("authors"):
+        fic.authors.add(author)
+
+    for ship in data.get("ships"):
+        fic.ships.add(ship)
+
+    for character in data.get("characters"):
+        fic.characters.add(character)
+
+    return fic
---
+++ fics/tests/test_commands.py
@@ -0,0 +1,63 @@
+from model_bakery import baker
+from test_plus.test import TestCase
+
+from fics.management.commands.constants import (
+    HARRY_POTTER_SHIPS,
+    HARRY_POTTER,
+    STAR_WARS,
+    SPY_FAMILY,
+    GILMORE_GIRLS,
+    FANDOMS,
+    CHARACTERS,
+    SHIPS,
+)
+from fics.management.commands.load_sample_fics import (
+    create_authors,
+    create_characters,
+    create_external_id,
+    create_fandoms,
+    create_ships,
+    create_url,
+    get_ships_from_fandom,
+)
+from fics import models
+
+
+class LoadSampleFicsTestCase(TestCase):
+    def test_create_authors(self):
+        count = models.Author.objects.count()
+        result = create_authors(1)
+        self.assertEquals(models.Author.objects.count(), count + 1)
+
+    def test_create_characters(self):
+        count = models.Character.objects.count()
+        result = create_characters(["Harry", "Hermione"])
+        self.assertEquals(models.Character.objects.count(), count + 2)
+        self.assertTrue(models.Character.objects.filter(name="Harry").exists())
+        self.assertTrue(models.Character.objects.filter(name="Hermione").exists())
+
+    def test_create_fandoms(self):
+        count = models.Fandom.objects.count()
+        result = create_fandoms(["Sample"])
+        self.assertEquals(models.Fandom.objects.count(), count + 1)
+        self.assertTrue(models.Fandom.objects.filter(name="Sample").exists())
+
+    def test_create_ships(self):
+        count = models.Ship.objects.count()
+        result = create_ships(["Sample", "Another sample"])
+        self.assertEquals(models.Ship.objects.count(), count + 2)
+        self.assertTrue(models.Ship.objects.filter(name="Sample").exists())
+
+    def test_create_external_id(self):
+        result = create_external_id()
+        self.assertEqual(type(result), str)
+
+    def test_create_url(self):
+        result = create_url("hermione")
+        self.assertEquals(result, "https://example.com/fics/hermione/")
+
+    # def test_get_ships_from_fandom(self):
+    #     expected = HARRY_POTTER_SHIPS
+    #     result = get_ships_from_fandom(HARRY_POTTER)
+    #     self.assertEquals(set(expected), set(result))
+
